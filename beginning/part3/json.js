e424eb81bbc1014e7ae5739f8c27a3722f45aaf0
gbilog <gerald.ilog@gmail.com>
2017-05-07 13:06:47 -0700

    Assignment10
    parts 1-4

---
+++ beginning/part3/json.js
@@ -0,0 +1,27 @@
+{"teammembers":[
+	{
+		"name":"Avatar Aang",
+		"title":"Vice President of Airbending",
+		"bio":"The newly found Avatar, master of all four elements. Aang has travelled around the world learning to bend the other elements: Fire, Water, and Earth."
+	},
+	{
+		"name":"Captain Planet",
+		"title":"Director of Ecological Preservation",
+		"bio":"Captain Planet answers the call to protect the Earth from those who would seek to pollute the planet and strip it of its natural resources. His aim is to preserve resources for future generations."
+	},
+	{
+		"name":"Professor X",
+		"title":"Director of Human Telepathy",
+		"bio":"Charles Xavier founded the Xavier School for Gifted Youngsters with four of his original students. As the worlds most powerful telepath, he is uniquely qualified to guide gifted young mutants along a path to control their powers."
+	},
+	{
+		"name":"Darryl Hall",
+		"title":"Vice President of Philadelphia Soul",
+		"bio":"Most known as one-half of the duo 'Hall & Oates', Darryl co-wrote many songs still popular today. He currently hosts a television show on the Paladium Network where he invites new artists to his private home and play alongside him and his band."
+	},
+	{
+		"name":"Wilbur",
+		"title":"Founder and CEO",
+		"bio":"While Wilbur is best known as the beneficiary of his friend Charlotte's web messages, he made quite a name for himself after avoiding the dinner table. He currently hosts a weekly podcast on the subject of turnkey business logistics."
+	}
+]}
